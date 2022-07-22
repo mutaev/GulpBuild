9037825bfd6c16f785662be11ef0ea778fcdc13d
MUtaev <eldarmutaev@inbox.ru>
2022-07-22 14:32:18 +0300

    first version

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+// подключение модулей 
+const gulp = require('gulp')
+const less = require('gulp-less')
+const rename = require('gulp-rename')
+const cleanCSS = require('gulp-clean-css')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const del = require('del')
+
+
+// пути к нашим файлом 
+const paths = {
+    styles: {
+        src: 'src/styles/**/*.less',
+        dest: 'dist/css/'
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js',
+        dest: 'dist/js/'
+    }
+}
+
+// очистка каталога 
+function clean() {
+    return del(['dist'])
+}
+
+// задача для обрабоки стилей
+function styles(){
+    return gulp.src(paths.styles.src)
+    .pipe(less())
+    .pipe(cleanCSS())
+    .pipe(rename({
+        basename: 'main',
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
+// задача для обработки скриптов
+function scripts(){
+    return gulp.src(paths.scripts.src, {
+        sourcemaps: true
+    })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest))
+}
+
+// для того чтобы стили и js  автоматически компилировались
+function watch(){
+    gulp.watch(paths.styles.src, styles)
+    gulp.watch(paths.scripts.src, scripts)
+}
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+
+exports.clean = clean
+exports.styles = styles
+exports.scripts =scripts
+exports.watch =  watch
+exports.build = build
+exports.default = build
